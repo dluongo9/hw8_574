bf6e4927014491800cb9095400016b74abbb5cea
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-06-03 16:40:25 -0700

    committed

--- src/run_hw8.sh
+++ src/run_hw8.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate /dropbox/21-22/575k/env
-python run.py
+python run.py --train_source /dropbox/21-22/575k/data/europarl-v7-es-en/train.en.txt --train_target /dropbox/21-22/575k/data/europarl-v7-es-en/train.es.txt --output_file test.en.txt.es --num_epochs 8 --embedding_dim 16 --hidden_dim 64 --num_layers 2
